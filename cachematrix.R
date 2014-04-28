e3fb20facce045818bfb4ce4d4b632096533bc3c
mindfulUniverse <mindfulUniverse>
2014-04-27 22:03:06 -0400

    assignment
    first assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  
+  setinversematrix <- function(inv) m <<- inv
+      
+  getinversematrix <- function() m
+  
+  list(set = set, get = get,
+       setinversematrix = setinversematrix,
+       getinversematrix = getinversematrix)
 
 }
-
-
-## Write a short comment describing this function
-
+## used ginv function from MASS library to calculate inverse
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  cm <- x$getinversematrix()
+  if(!is.null(cm)) {
+    message("getting cached matrix")
+    return(cm)
+  }
+  origmat <- x$get()
+  insmat<-ginv(origmat, ...)
+  ##cm <- inver(insmat, ...)
+  x$setinversematrix(insmat)
+  insmat
 }
